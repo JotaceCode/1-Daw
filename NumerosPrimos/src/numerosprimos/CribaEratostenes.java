a580f575ac91e188c79577e581309b8241d1c6c9
Jose Carlos <Josvelsei@gmail.com>
2023-02-18 11:11:10 +0100

    criba refactorizada

--- NumerosPrimos/src/numerosprimos/CribaEratostenes.java
+++ NumerosPrimos/src/numerosprimos/CribaEratostenes.java
@@ -1,22 +1,6 @@
 /**
  * Clase para generar todos los números primos de 1 hasta
- * un número máximo especificado por el usuario. Como 
- * algoritmo se utiliza la criba de Eratóstenes.
- * 
- * Eratóstenes de Cirene (276 a.C., Cirene, Libia – 194
- * a.C., Alejandría, Egipto) fue el primer hombre que 
- * calculó la circunferencia de la Tierra. También
- * se le conoce por su trabajo con calendarios que ya 
- * incluían años bisiestos y por dirigir la mítica 
- * biblioteca de Alejandría.
- * 
- * El algoritmo es bastante simple: Dado un vector de
- * enteros empezando en 2, se tachan todos los múltiplos
- * de 2. A continuación, se encuentra el siguiente
- * entero no tachado y se tachan todos sus múltiplos. El 
- * proceso se repite hasta que se pasa de la raíz cuadrada
- * del valor máximo. Todos los números que queden sin 
- * tachar son números primos.
+ * un número máximo especificado por el usuario. 
  */ 
 
 package numerosprimos;    
